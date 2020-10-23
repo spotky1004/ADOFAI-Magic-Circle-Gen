a3894c7b262358be7415dd0d93e324eee9fba94a
Spotky1004 <spotky1004@gmail.com>
2020-10-24 01:54:22 +0900

    visual update (from my other repo)

--- index.js
+++ index.js
@@ -3,9 +3,91 @@ document.querySelector('#genButton').onclick = new Function(`
     cutNumber(document.querySelector('#tileCount').innerHTML),
     cutNumber(document.querySelector('#perCount').innerHTML),
     cutNumber(document.querySelector('#twirlDrict').innerHTML),
-    cutNumber(document.querySelector('#midspinFlag').innerHTML)
+    cutNumber(document.querySelector('#midspinFlag').innerHTML),
+    360
   );`
 );
+
+setInterval( function () {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  drawCanvas();
+  keyLoop();
+}, 50);
+
+univScale = 0.2;
+absPos = [0, 0];
+map = new ADOFAI();
+function drawCanvas() {
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.beginPath();
+  c.rect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = '#000';
+  c.fill();
+  unitLeng = canvas.width/100*univScale;
+  var tileLeng = 2;
+  var posNow = [canvas.width/2+canvas.width*absPos[0], canvas.height/2+canvas.width*absPos[1]];
+  var lastDeg = 90;
+  for (var i = 0; i < map.pathData.length; i++) {
+    c.beginPath();
+    c.lineWidth = 0.00001;
+    c.strokeStyle = "#debb7b";
+    c.arc(posNow[0], posNow[1], unitLeng, 0, Math.PI*2);
+    c.stroke();
+    c.fillStyle = "#debb7b";
+    c.fill();
+    c.beginPath();
+    c.moveTo(posNow[0], posNow[1]);
+    c.lineTo(posNow[0]+tileLeng*Math.sin(Math.rad((lastDeg+180)%360))*unitLeng, posNow[1]-tileLeng*Math.cos(Math.rad((lastDeg+180)%360))*unitLeng);
+    c.lineWidth = unitLeng*2;
+    c.stroke();
+    c.beginPath();
+    c.moveTo(posNow[0], posNow[1]);
+    c.lineTo(posNow[0]+tileLeng*Math.sin(Math.rad((map.pathData[i].absoluteAngle+270)%360))*unitLeng, posNow[1]-tileLeng*Math.cos(Math.rad((map.pathData[i].absoluteAngle+270)%360))*unitLeng);
+    c.lineWidth = unitLeng*2;
+    c.stroke();
+    posNow = [posNow[0]+(tileLeng*2+0.2)*Math.sin(Math.rad((map.pathData[i].absoluteAngle+270)%360))*unitLeng, posNow[1]-(tileLeng*2+0.2)*Math.cos(Math.rad((map.pathData[i].absoluteAngle+270)%360))*unitLeng];
+    lastDeg = (map.pathData[i].absoluteAngle+270)%360;
+  }
+}
+function keyLoop() {
+  var screenSpeed = 0.005;
+  if (keyCode[37]) absPos[0] += screenSpeed;
+  if (keyCode[39]) absPos[0] -= screenSpeed;
+  if (keyCode[38]) absPos[1] += screenSpeed;
+  if (keyCode[40]) absPos[1] -= screenSpeed;
+}
+
+canvas = document.querySelector('#canvas');
+c = canvas.getContext('2d');
+
+keyCode = {};
+function zoom(event) {
+  univScale *= (1+-Math.sign(event.deltaY)/33);
+}
+window.onwheel = zoom;
+keyNow = [];
+(function(){
+  document.addEventListener('keydown', function(e){
+    keyCode[e.keyCode] = 1;
+    keyNow = e.keyCode;
+    if (keyNow == 49) map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, 90)]));
+    if (keyNow == 50) map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, 180)]));
+    if (keyNow == 51) map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, 270)]));
+    if (keyNow == 52) map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, 360)]));
+    if (keyNow == 53) map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, 45)]));
+    if (keyNow == 54) map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, 135)]));
+    if (keyNow == 55) map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, 225)]));
+    if (keyNow == 56) map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, 315)]));
+  })
+  document.addEventListener('keyup', function(e){
+    keyCode[e.keyCode] = 0;
+  })
+})();
+
+Math.rad = function(degrees) {
+  return degrees * Math.PI / 180;
+};
 
 function cutNumber(a) {
   if (!isNaN(Number(a))) {
--- module.js
+++ module.js
@@ -1,4 +1,5 @@
 import ADOFAI from "./ADOFAI-WebModule/ADOFAI-WebModule.js";
+window.ADOFAI = ADOFAI;
 
 function findIndex(arr, toFind) {
   for (var i = 0; i < arr.length; i++) {
@@ -8,97 +9,104 @@ function findIndex(arr, toFind) {
   }
   return -1;
 }
-function generateCircle(leng=-1, perC=-1, twirl=0, midspin=0) {
-  var map = new ADOFAI();
-  map.pathData = [];
-  map.pathData.push(new ADOFAI.PathData('R'));
-  map.pathData.push(new ADOFAI.PathData('R'));
+function generateCircle(leng=-1, perC=-1, twirl=0, midspin=0, maxoffset=360) {
+  try {
+    var map = new ADOFAI();
+    map.pathData = [];
+    map.pathData.push(new ADOFAI.PathData('R'));
+    map.pathData.push(new ADOFAI.PathData('R'));
 
-  if (!(twirl == 0 || twirl == 1)) {
-    twirl = Math.floor(Math.random()*2);
-  }
-  if (twirl) {
-    map.actions.push(new ADOFAI.Action(1, "Twirl"));
-  }
-  if (!(midspin == 0 || midspin == 1)) {
-    midspin = Math.floor(Math.random()*2);
-  }
-  if (perC == -1 || !(perC == 2 || perC == 3 || perC == 4 || perC == 6)) {
-    var per = ((Math.floor(Math.random()*2)) ? ((Math.floor(Math.random()*2)) ? 2 : 3) : ((Math.floor(Math.random()*2)) ? 4 : 6));
-  } else {
-    var per = perC;
-  }
-  if (leng == -1) {
-    var perLeng = Math.floor(Math.random()*100+100);
-  } else {
-    var perLeng = Math.ceil(leng/per);
-  }
-
-  var perArr = [];
-  var totDeg = 0;
-  for (var i = 0; i < perLeng; i++) {
-    if (i == perLeng-1 && totDeg%(360/per) != 0) {
-      perArr.splice(perArr.length-5, perArr.length-1);
-      i -= 5;
+    if (maxoffset > 360 || maxoffset < 15) {
+      maxoffset = 360;
     }
-    pushPerDeg();
-  }
-
-  for (var i = 0; i < per; i++) {
-    for (var j = 0; j < perArr.length; j++) {
-      map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, (perArr[j]+360/per*i)%360)]));
-      if (
-        midspin &&
-        findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, (perArr[j]+360/per*i-15)%360) != -1 &&
-        getDeg(perArr[j], perArr[j+1]) > 15
-      ) {
-        map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, (perArr[j]+360/per*i-15)%360)]));
-        map.pathData.push(new ADOFAI.PathData('!'));
-      }
+    if (!(twirl == 0 || twirl == 1)) {
+      twirl = Math.floor(Math.random()*2);
     }
-  }
-
-  console.log(map);
-  var prevDeg = 180;
-  var prevOffest = 180;
-  for (var i = 1; i < map.pathData.length; i++) {
-    map.actions.push(new ADOFAI.Action((twirl ? i+1 : i), "SetSpeed"));
-    map.actions[i-1].eventValue.isSpeedTypeBPM = false;
-    if (!(map.pathData[i].code == '!' || map.pathData[i-1].code == '!')) {
-      var multThis = (getDeg(prevDeg, map.pathData[i].absoluteAngle)/prevOffest);
-      map.actions[i-1].eventValue.BPM_Multiplier = multThis;
-      prevOffest = getDeg(prevDeg, map.pathData[i].absoluteAngle);
-      prevDeg = map.pathData[i].absoluteAngle;
+    if (twirl) {
+      map.actions.push(new ADOFAI.Action(1, "Twirl"));
+    }
+    if (!(midspin == 0 || midspin == 1)) {
+      midspin = Math.floor(Math.random()*2);
+    }
+    if (perC == -1 || !(perC == 2 || perC == 3 || perC == 4 || perC == 6)) {
+      var per = ((Math.floor(Math.random()*2)) ? ((Math.floor(Math.random()*2)) ? 2 : 3) : ((Math.floor(Math.random()*2)) ? 4 : 6));
     } else {
-      if (map.pathData[i].code == '!') {
-        map.actions[i-1].eventValue.BPM_Multiplier = 0.1;
-      } else {
-        map.actions[i-1].eventValue.BPM_Multiplier = 10;
-      }
+      var per = perC;
     }
-  }
+    if (leng == -1) {
+      var perLeng = Math.floor(Math.random()*100+100);
+    } else {
+      var perLeng = Math.ceil(leng/per);
+    }
 
-  map.settings.backgroundColor = '000000';
-  map.settings.trackColor = 'debb7b';
+    var perArr = [];
+    var totDeg = 0;
+    for (var i = 0; i < perLeng; i++) {
+      if (i == perLeng-1 && totDeg%(360/per) != 0) {
+        perArr.splice(perArr.length-5, perArr.length-1);
+        i -= 5;
+      }
+      pushPerDeg();
+    }
 
-  document.querySelector('#genOutput').innerHTML = map.Export();
-
-  function pushPerDeg() {
-    var degThis = ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[Math.floor((ADOFAI.PathData.ABSOLUTE_ANGLE_LIST.length-5)*Math.random())];
     for (var i = 0; i < per; i++) {
-      if ((degThis+360/per*i)%360 == 0 || (degThis-perArr[perArr.length-1]+720)%360 == 180 || getDeg(degThis, perArr[perArr.length-1]) == 360 || findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, (degThis+360/per*i)%360) == -1) {
-        pushPerDeg();
-        return;
+      for (var j = 0; j < perArr.length; j++) {
+        map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, (perArr[j]+360/per*i)%360)]));
+        if (
+          midspin &&
+          findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, (perArr[j]+180+(twirl ? -1 : 1)*15)%360) != -1
+        ) {
+          map.pathData.push(new ADOFAI.PathData(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, (perArr[j]+180+(twirl ? -1 : 1)*15)%360)]));
+          map.pathData.push(new ADOFAI.PathData('!'));
+        }
       }
     }
-    totDeg += degThis;
-    perArr.push(degThis);
-  }
-  function getDeg(d1, d2) {
-    var degOff = (d2-d1+540)%360;
-    if (twirl) degOff = 360-degOff;
-    if (degOff == 0) degOff = 360;
-    return degOff;
+
+    console.log(map);
+    var prevDeg = 180;
+    var prevOffest = 180;
+    for (var i = 1; i < map.pathData.length; i++) {
+      map.actions.push(new ADOFAI.Action((twirl ? i+1 : i), "SetSpeed"));
+      map.actions[i-1].eventValue.isSpeedTypeBPM = false;
+      if (!(map.pathData[i].code == '!' || map.pathData[i-1].code == '!')) {
+        var multThis = (getDeg(prevDeg, map.pathData[i].absoluteAngle)/prevOffest);
+        map.actions[i-1].eventValue.BPM_Multiplier = multThis;
+        prevOffest = getDeg(prevDeg, map.pathData[i].absoluteAngle);
+        prevDeg = map.pathData[i].absoluteAngle;
+      } else {
+        if (map.pathData[i].code == '!') {
+          map.actions[i-1].eventValue.BPM_Multiplier = 0.1;
+        } else {
+          map.actions[i-1].eventValue.BPM_Multiplier = 10;
+        }
+      }
+    }
+
+    map.settings.backgroundColor = '000000';
+    map.settings.trackColor = 'debb7b';
+
+    document.querySelector('#genOutput').innerHTML = map.Export();
+    window.map = map;
+
+    function pushPerDeg() {
+      var degThis = ADOFAI.PathData.ABSOLUTE_ANGLE_LIST[Math.floor((ADOFAI.PathData.ABSOLUTE_ANGLE_LIST.length-5)*Math.random())];
+      for (var i = 0; i < per; i++) {
+        if ((degThis+360/per*i)%360 == 0 || (degThis-perArr[perArr.length-1]+720)%360 == 180 || getDeg(degThis, perArr[perArr.length-1]) == 360 || findIndex(ADOFAI.PathData.ABSOLUTE_ANGLE_LIST, (degThis+360/per*i)%360) == -1) {
+          pushPerDeg();
+          return;
+        }
+      }
+      totDeg += degThis;
+      perArr.push(degThis);
+    }
+    function getDeg(d1, d2) {
+      var degOff = (d2-d1+540)%360;
+      if (twirl) degOff = 360-degOff;
+      if (degOff == 0) degOff = 360;
+      return degOff;
+    }
+  } catch (e) {
+    document.querySelector('#genOutput').innerHTML = 'bug!\n' + e;
   }
 }
 
